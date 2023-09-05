e0e8288586c404d729f3f12d45b8a1879a3e36dd
WillianMGBrdotnet <willianmgbr@hotmail.com>
2023-09-05 19:43:17 -0300

    Anexar Tabela Linear

--- Program.cs
+++ Program.cs
@@ -1,19 +1,30 @@
-﻿using System;
-
-namespace algoritmos01
+﻿//Adicionar um valor no final do array
+public class Program
 {
-    class Program
+    static int[] Add(int[] array, int value)
     {
-        static void Main(string[] args)
+    int[] temporarioArray = new int[array.Length + 1];
+
+    for(int i = 0; i < array.Length; i++)
+    {
+        temporarioArray[i] = array[i];
+    }
+    //o indice com valor igual ao tamanho do array real, receberá o novo valor
+    temporarioArray[array.Length] = value;
+
+    return temporarioArray;
+    }
+
+    static void Main(string[] args)
+    {
+        int[] array = { 90, 70, 50, 80, 60, 85 };
+        //chamando o método que adiciona um espaço no final do array e preenche com um valor
+        array = Add(array, 75);
+
+        for (int i = 0; i < array.Length; i++)
         {
-            int[] array1 = { 90, 70, 50, 80, 60, 85 };
-
-            for (int i = 0; i < array1.Length; i++)
-            {
-                System.Console.Write(array1[i] + ",");
-            }
-            //90,70,50,80,60,85,
-
+        System.Console.Write(array[i] + ",");
         }
     }
 }
+
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Estrutura de dados e Algoritmos com C#
 
 ## Tabela Linear
+
+## Anexar Tabela Linear
+Adiciona um valor no final do array
