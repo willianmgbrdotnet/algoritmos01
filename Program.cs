96d35aeeef51f6216a3b0c7542eef89675910b23
WillianMGBrdotnet <willianmgbr@hotmail.com>
2023-09-05 04:14:24 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace algoritmos01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 90, 70, 50, 80, 60, 85 };
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Estrutura de dados e Algoritmos com C#
+
+## Tabela Linear
